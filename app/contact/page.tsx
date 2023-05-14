afdea93372073753a26d689efa9419e36753e976
Tae Yeong Kim <53635845+TaetaeMVS@users.noreply.github.com>
2023-05-15 04:11:40 +1000

    remove twitter

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,11 @@
 "use client";
-import { Github, Mail, Twitter, Linkedin } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
-import { Anek_Bangla } from "@next/font/google";
 
 const socials = [
-	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/taeyeong2000",
-		label: "Twitter",
-		handle: "@taeyeong2000",
-	},
+
 	{
 		icon: <Mail size={20} />,
 		href: "mailto:taeyeong2000@gmail.com",
@@ -38,7 +32,7 @@ export default function Example() {
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-4 mx-auto mt-32 sm:mt-0 sm:grid-cols-4 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
 							<Link
